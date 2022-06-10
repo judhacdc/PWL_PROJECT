8ef018e8eb2a34217beca58eb32949ef2706bc4f
judhacdc <jdxxaa@gmail.com>
2022-06-10 16:00:57 +0700

    add category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,83 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+        public function __construct()
+        {
+        $this->middleware('auth');
+        }
+
     public function index()
     {
-        //
+        $categories = Category::paginate(10);
+
+        return view('dashboard.category.index',[
+            'categories' => $categories
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function create(){
+        return view('dashboard.category.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nama' => 'required'
+        ]);
+
+        $category = new Category;
+
+        $category->nama = $request->get('nama');
+        $category->slug = SlugService::createSlug(Category::class,'slug',$request->nama);
+
+        $category->save();
+
+        return redirect()->route('category.index')->with('success','category Ditambahkan');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function delete($id){
+        $category = Category::where('id', $id)->first();
+
+        Category::find($category->id)->delete();
+
+        return redirect()->route('category.index')->with('success','Category berhasil dihapus');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    // public function checkSlug(Request $request){
+    //     $slug = SlugService::createSlug(Post::class, 'slug',$request->nama);
+    //     return response()->json(['slug' => $slug]);
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }

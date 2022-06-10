e6aa9429978562396a194a1ff76c5d526d5f7ab9
judhacdc <jdxxaa@gmail.com>
2022-06-10 16:06:43 +0700

    update payment

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -32,4 +32,8 @@ class Product extends Model
         return $this->belongsTo(Category::class,'category_id');
     }
 
+    public function OrderDetail(){
+        return $this->hasMany(OrderDetail::class);
+    }
+
 }
--- resources/views/layouts/dashboard/topbar.blade.php
+++ resources/views/layouts/dashboard/topbar.blade.php
@@ -1,13 +1,13 @@
-            <!-- Topbar -->
-            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+<!-- Topbar -->
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                <!-- Sidebar Toggle (Topbar) -->
-                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                    <i class="fa fa-bars"></i>
-                </button>
+    <!-- Sidebar Toggle (Topbar) -->
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <i class="fa fa-bars"></i>
+    </button>
 
-                <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
+    <!-- Topbar Navbar -->
+    <ul class="navbar-nav ml-auto">
 
                     <!-- Nav Item - User Information -->
                     <li class="nav-item dropdown no-arrow">
@@ -19,7 +19,7 @@
                 </a>
                     </li>
 
-                </ul>
+    </ul>
 
-            </nav>
-            <!-- End of Topbar -->
+</nav>
+<!-- End of Topbar -->
